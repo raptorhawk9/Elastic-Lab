237db504a8c36e762828fa30a450e8b3bfe965dc
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-12-14 09:26:21 -0800

    Add dropdown

--- src/main/java/frc/robot/subsystems/Info.java
+++ src/main/java/frc/robot/subsystems/Info.java
@@ -5,22 +5,36 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Info extends SubsystemBase {
   private int meaningOfLife;
   private boolean progIsBest;
   private Field2d field;
+  private SendableChooser<String> select;
 
   /** Creates a new Info. */
   public Info() {
     meaningOfLife = 42;
     progIsBest = true;
     field = new Field2d();
+
+    select = new SendableChooser<>();
+    select.addOption("prog", "prog");
+    select.addOption("systems", "systems");
+    select.addOption("cad", "cad");
+    select.addOption("mech", "mech");
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Meaning of Life", meaningOfLife);
+    SmartDashboard.putBoolean("Prog is best? ", progIsBest);
+    SmartDashboard.putData("Field", field);
+    SmartDashboard.putData("best subgroup", select);
+
+    System.out.println(select.getSelected());
   }
 }
